60d1528d490517c6258cfa2518fe94ff13511a05
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-02-23 21:31:49 +0300

    update task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp/school with specific permissions, owner, group, and content
 file { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => "I love Puppet\n",
 }
