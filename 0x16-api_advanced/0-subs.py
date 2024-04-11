adc93ce39ad46effc4f450fb09c1a98a86e5b83e
bini34 <biniyamambachew@gmail.com>
2024-04-11 23:16:31 +0300

    update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python:alx-system_engineering-devops:v1.0 (by /u/username)'}
+    headers = {'User-Agent':
+               'Python:alx-system_engineering-devops:v1.0 (by /u/username)'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get("data", {}).get("subscribers", 0)
